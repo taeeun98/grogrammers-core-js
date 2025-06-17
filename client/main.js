4ff71318f26bd5f90ce3bd8e08fe4a611f8c053c
taeeun98 <xodms6360@naver.com>
2025-06-17 16:04:02 +0900

    랜덤다이스

--- client/main.js
+++ client/main.js
@@ -1,97 +1,119 @@
-import {
-  copy,
-  shake,
-  addClass,
-  getRandom,
-  showAlert,
-  insertLast,
-  removeClass,
-  getNode as $,
-  clearContents,
-  isNumericString,
-} from './lib/index.js';
+import { diceAnimation, getNode } from './lib/index.js';
+// import organize 개발자 검색창에 쳐서 적용하면 됨
+// shift alt O
 
-import data from './data/data.js';
 
-/* 
+/*
+1. 주사위 굴리기 버튼을 선택
+   - querySelector or getNode
 
-[phase-1]
+2. 클릭 이벤트 바인딩
+   - click
 
-1. 주접 떨기 버튼을 클릭하는 함수
-    - 주접 떨기 버튼 가져오기
-    - 이벤트 연결 'click'
+3. 버튼 클릭시 diceAnimation 애니메이션 동작
+   - setInterval diceAnimation
 
-2. input 값 가져오기 
-    - input.value
+4. 같은 버튼을 눌렀을 때 토글 처리
+   - 상태 변수 만들기
+     - isClicked = false;
+     - isClicked = !isClicked;
+   - 조건 처리
 
-3. data 함수에서 주접 이름 넣고 꺼내기 => [] 리턴값 확인
-    - n번째 주접 랜덤 pick 하기 
-
-4. result에 렌더링 하기
-    - insertLast 
-
-[phase-2]
-5. 예외 처리
-    - 이름이 없을 경우 에러
-    - 숫자만 들어오면 에러
-
+5. 애니메이션 재생 or 정지
+   - setInterval
+   - clearInterval
 */
 
-const submit = $('#submit');
-const nameField = $('#nameField');
-const result = $('.result');
+// 구조분해할당
+const [rollingButton, recordButton, resetButton] = document.querySelectorAll(
+  '.buttonGroup button'
+);
+
+const recordListWrapper = getNode('.recordListWrapper');
+let num = 0;
+let total = 0;
 
 
-function handleSubmit(e) {
-  e.preventDefault();
 
-  const name = nameField.value;
-  const list = data(name);
-  const pick = list[getRandom(list.length)];
+// function handleRollingDice() {
+//   // 전역변수를 줄이기 위한 클로저
+//   let isClicked = false;
+//   let id;
+//   // 클로저를 위한 return 
+//   return () => {
+//     if (!isClicked) {
+//       id = setInterval(diceAnimation, 100);
+//     } else {
+//       clearInterval(id);
+//     }
+//     isClicked = !isClicked;
+//   };
+// }
+// rollingButton.addEventListener('click', handleRollingDice());
+
+//addEventListener에 () 없애려면 함수를 바로 실행하면 됨.
+const handleRollingDice = (() => {
+
+  // 전역변수를 줄이기 위한 클로저
+  let isClicked = false;
+  let id;
+
+  // 클로저를 위한 return 
+  return () => {
+    if (!isClicked) {
+      id = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+      resetButton.disabled = true;
+    } else {
+      clearInterval(id);
+      recordButton.disabled = false;
+      resetButton.disabled = false;
+    }
+
+    isClicked = !isClicked;
+  };
+})();
 
 
-  if(!name || name.replaceAll(' ','') === ''){
-
-    showAlert({
-      target:'.alert-error',
-      message:'공백은 허용되지 않습니다.',
-      timeout:2000,
-      className:'is-active'
-    })
-
-    shake(nameField)
-    return;
-  }
-
-  if(!isNumericString(name)){
-    showAlert({
-      target:'.alert-error',
-      message:'정확한 이름을 입력해 주세요.',
-      timeout:2000,
-      className:'is-active'
-    })
-
-    shake(nameField)
-    return;
-  }
-
-  clearContents(result);
-  insertLast(result, pick);
+function createItem(dice) {
+  return /* html */`
+    <tr>
+      <td>${++num}</td>
+      <td>${dice}</td>
+      <td>${total += Number(dice)}</td>
+    </tr>
+  `
 }
 
-function handleCopyClipboard() {
-  const text = this.textContent;
 
-  copy(text)
-    .then(() => {
-      showAlert({
-        target: '.alert-success',
-        className: 'is-active',
-        message: '클립보드 복사 완료!!',
-        timeout: 2000,
-      });
-    });
+function renderRecordItem() {
+  const dice = document.querySelector('#cube').getAttribute('dice');
+  const html = createItem(dice);
+
+  document.querySelector('.recordList tbody').insertAdjacentHTML( 'beforeend', html);
+  recordListWrapper.scrollTop = recordListWrapper.scrollHeight;
 }
 
-submit.addEventListener('click', handleSubmit);
-result.addEventListener('click', handleCopyClipboard);
+
+function handleRecord() {
+  recordListWrapper.hidden = false;
+  renderRecordItem();
+}
+
+
+function resetRecord() {
+  num = 0;
+  total = 0;
+  document.querySelector('.recordList tbody').innerHTML = '';
+}
+
+
+function handleReset() {
+  recordListWrapper.hidden = true;
+  resetRecord();
+}
+
+
+rollingButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);
